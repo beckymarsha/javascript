aa0181062a61309707fabdc000a028012064f4e3
Becky Marshall <becky.marshall@id.iit.edu>
2017-03-31 12:03:20 -0500

    end state of javascript stuff with a nonworking ‘typewriter’ effect

--- script.js
+++ script.js
@@ -10,10 +10,19 @@ var moveAmount = 20;
 
 function boxClicked() {
     console.log("Your box has been clicked")
+
+    if (leftMargin < 100) {
+        leftMargin += moveAmount;
+    } else {
+        leftMargin -= moveAmount;
+    }
+
     leftMargin += moveAmount;
     topMargin += moveAmount;
+
     boxes[0].style.marginLeft = leftMargin + "px";
     boxes[0].style.marginTop = topMargin + "px";
+
 }
 
 boxes[0].addEventListener("click", boxClicked)
