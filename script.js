605da22b74eaeae280ff847780dca3ac042999a1
Becky Marshall <becky.marshall@id.iit.edu>
2017-03-31 11:40:00 -0500

    Created click event listener for first box

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var boxes = document.querySelectorAll(".box");
 
 boxes[0].style.backgroundColor = "blue";
+
 boxes[2].style.backgroundColor = "yellow";
+
+function boxClicked() {
+    console.log("Your box has been clicked")
+}
+
+boxes[0].addEventListener("click", boxClicked)
 
 console.log(boxes[0]);
