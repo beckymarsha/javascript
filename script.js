7816abdb39b128337fddf41871d52378305f911c
Becky Marshall <becky.marshall@id.iit.edu>
2017-03-31 11:55:17 -0500

    added topMargin motion

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ boxes[0].style.backgroundColor = "blue";
 
 boxes[2].style.backgroundColor = "yellow";
 
+var leftMargin = 00;
+var topMargin = 00;
+var moveAmount = 20;
+
 function boxClicked() {
     console.log("Your box has been clicked")
+    leftMargin += moveAmount;
+    topMargin += moveAmount;
+    boxes[0].style.marginLeft = leftMargin + "px";
+    boxes[0].style.marginTop = topMargin + "px";
 }
 
 boxes[0].addEventListener("click", boxClicked)
